2b2dde74b80875a0d98aaea5f35d95550d6d5553
Abhishek <abhishekchoudhary45@gmail.com>
2021-05-18 11:59:07 +0530

    visual studio use

---
+++ HOW TO/visual_stu.py
@@ -0,0 +1,22 @@
+#install python : to get some additional functionality 
+#install run : to get run code
+#Kerboard shortcut change
+
+#user setting : setting for full interface
+#Workspace setting : Setting for just the folder
+
+#change setting : 
+# type "code runner" and change:-
+# 1."clear output before each run", a new setting,json will be created
+# 2. Run in terminal : so that our code can take input
+
+#Tricks
+#1. Use Alt key to create multiple cursor as well as remove multiple cursors
+#2. You can create your own user snippets like print fstring
+#3. Single click to "preview" / italic and double click to "open"
+#4. View --> Editor Layout to split screen
+#5. Shift+right+Powershell, code -h, code -d 1.txt 2txt : this will show the difference between 2 files,dislayed on visual studio
+#6  To open visual studio code "code ."
+
+#Using user stippet
+print("Hello ") 
